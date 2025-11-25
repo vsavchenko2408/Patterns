4df647c7a5e760d354ece45d9dc8d20e890096cc
P15s <v.savchenko2408@gmail.com>
2025-11-25 10:43:49 +0100

    Realise Singleton

--- Singleton/src/main.cpp
+++ Singleton/src/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
+class Singleton
+{
+    private:
+    static Singleton* object;
+    Singleton()
+    {
+        std::cout << "Object created" << std::endl;
+    }
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    public:
+
+    static Singleton* create_obj()
+    {
+        if(object == nullptr)
+        {
+            object = new Singleton();
+        }
+        return object;
+    }
+
+    template<typename T>
+    void some_work(T& work)
+    {
+        std::cout << "Some work with: " << work << std::endl;
+    }
+};
+
+    Singleton* Singleton::object = nullptr;
 
 int main()
 {
-    
+    Singleton* obj = Singleton::create_obj();
+    obj->some_work("some data");    
 }
