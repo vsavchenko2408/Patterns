a5afc2648d5a3cad5ac416e3f770038e16165567
Der Meister <v.savchenko2408@gmail.com>
2025-12-02 23:19:49 +0100

    Video Singletone

--- Video/src/main.cpp
+++ Video/src/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
-
+class Singleton
+{
+    private:
+        Singleton()
+        {
+        std::cout << "Singleton is make!" << std::endl;
+        }
+        static Singleton* object;
+    public:
+        Singleton(const Singleton&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+        static Singleton* get_object()
+        {
+            if(object == nullptr)
+            {
+                object = new Singleton();
+            }
+            return object;
+        }
+        void something_doing()
+        {
+            std::cout << "Some work!" << std::endl;
+        }
+};
+    Singleton* Singleton::object = nullptr;
 int main()
 {
-    
+   Singleton* obj = Singleton::get_object(); 
+   obj->something_doing();
+
+
+   delete obj;
 }
