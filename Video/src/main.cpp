ef7f1daa0bd4068453d09b838a981c057ccc923e
Der Meister <v.savchenko2408@gmail.com>
2025-12-03 21:39:04 +0100

    Prototype in video

--- Video/src/main.cpp
+++ Video/src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+class Prototype
+{
+    private:
+    int m_data;
+    public:
+    Prototype() : m_data(0){}
+    Prototype(int data): m_data(data) {}
+    int get_data(){return m_data;}
+    Prototype* clone()
+    {
+        return new Prototype(*this);
+    }
+};
 
 int main()
 {
-
+    Prototype obj1(10);
+    Prototype* clone = obj1.clone();
+    std::cout << "Data in clone: " << clone->get_data() << std::endl;
+    delete clone;
 }
