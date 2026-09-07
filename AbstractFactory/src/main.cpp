bdd5110bdc9980f67d0edcaf06a4a44e48341c88
Der Meister <v.savchenko2408@gmail.com>
2026-09-07 02:20:11 +0200

    Abstract Factory

--- AbstractFactory/src/main.cpp
+++ AbstractFactory/src/main.cpp
@@ -1,37 +1,139 @@
 #include <iostream>
 
-class Mage
+class Enemy
 {
-
+    public: 
+    virtual void Attack() = 0;
+    virtual ~Enemy() = default;
 };
 
-class Warrior
+class Weapon
 {
-
+    public:
+    virtual void Use() = 0;
+    virtual ~Weapon() = default;
 };
 
-class Archery
+class Mage: public Enemy
 {
-
+    public:
+    void Attack() override
+    {
+        std::cout << "Fireball!" << std::endl;
+    }
 };
 
-class Orcs
+class Warrior: public Enemy
 {
-    
+    public:
+    void Attack() override
+    {
+        std::cout << "Sword!" << std::endl;
+    }
 };
 
-class Elves
+class Archer: public Enemy
 {
-
+    public:
+    void Attack() override
+    {
+        std::cout << "Arrow!" << std::endl;
+    }
 };
 
-class Humans
+class Sword: public Weapon
 {
-
+    public:
+    void Use() override
+    {
+        std::cout << "Sword" << std::endl;
+    }
 };
 
+class Staff: public Weapon
+{
+    public:
+    void Use() override
+    {
+        std::cout << "Staff" << std::endl;
+    }
+};
+
+class Bow: public Weapon
+{
+    public:
+    void Use() override
+    {
+        std::cout << "Bow" << std::endl;
+    }
+};
+
+class EnemyFactory //Abstract Factory
+{
+    public:
+    virtual Enemy* CreateEnemy() = 0;
+    virtual Weapon* CreateWeapon() = 0;
+    virtual ~EnemyFactory() = default;
+};
+
+class MageFactory : public EnemyFactory
+{
+    public:
+    Enemy* CreateEnemy() override
+    {
+        return new Mage();
+    }
+    Weapon* CreateWeapon() override
+    {
+        return new Staff();
+    }
+};
+
+class WarriorFactory: public EnemyFactory
+{
+    public:
+    Enemy* CreateEnemy() override
+    {
+        return new Warrior();
+    }
+    Weapon* CreateWeapon() override
+    {
+        return new Sword();
+    }
+};
+
+class ArcherFactory: public EnemyFactory
+{
+    public:
+    Enemy* CreateEnemy() override
+    {
+        return new Archer();
+    }
+    Weapon* CreateWeapon() override
+    {
+        return new Bow();
+    }
+};
+
+void SpawnAndAttack(EnemyFactory* factory)
+{
+    Enemy* enemy = factory->CreateEnemy();
+    Weapon* weapon = factory->CreateWeapon();
+    enemy->Attack();
+    weapon->Use();
+    delete enemy;
+    delete weapon;
+}
 int main()
 {
-    std::cout << "Must be this text!" << std::endl;
-    return 0;
+    EnemyFactory* factory = new MageFactory();
+    SpawnAndAttack(factory);
+
+    factory = new WarriorFactory();
+    SpawnAndAttack(factory);
+
+    factory = new ArcherFactory();
+    SpawnAndAttack(factory);
+
+    delete factory;
 }
